aae44f9c3beeca82f9ed3248bb45ad01000a3bda
“Shubham” <shubhampattar2@gmail.com>
2020-06-29 17:27:43 +0530

    LOADED The Project For First Time

---
+++ src/main/java/com/bridgelabz/censusanalyser/IndiaCensusCSV.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.censusanalyser;
+
+import com.opencsv.bean.CsvBindByName;
+
+public class IndiaCensusCSV {
+
+    @CsvBindByName(column = "State", required = true)
+    public String state;
+
+    @CsvBindByName(column = "Population", required = true)
+    public int population;
+
+    @CsvBindByName(column = "AreaInSqKm", required = true)
+    public int areaInSqKm;
+
+    @CsvBindByName(column = "DensityPerSqKm", required = true)
+    public int densityPerSqKm;
+
+    @Override
+    public String toString() {
+        return "IndiaCensusCSV{" +
+                "State='" + state + '\'' +
+                ", Population='" + population + '\'' +
+                ", AreaInSqKm='" + areaInSqKm + '\'' +
+                ", DensityPerSqKm='" + densityPerSqKm + '\'' +
+                '}';
+    }
+}
